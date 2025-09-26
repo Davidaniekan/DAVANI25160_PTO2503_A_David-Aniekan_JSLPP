9a3b84ce110c169b687dcb42993146ec49093b77
David Aniekan <203154494+Davidaniekan@users.noreply.github.com>
2025-09-26 13:24:09 +0200

    Implement showError to display error message on board

--- scripts/ui.js
+++ scripts/ui.js
@@ -22,3 +22,19 @@ export function hideLoading() {
   const loadingDiv = document.getElementById("loading-message");
   if (loadingDiv) loadingDiv.remove();
 }
+
+/**
+ * Show an error message at the top of the columns.
+ * @param {string} message - Error message to display.
+ */
+export function showError(message) {
+  const existingError = document.getElementById("error-message");
+  if (existingError) existingError.remove();
+
+  const errorDiv = document.createElement("p");
+  errorDiv.textContent = message;
+  errorDiv.id = "error-message";
+  errorDiv.classList.add("error-div");
+  const columnsDiv = document.querySelector(".columns");
+  columnsDiv.prepend(errorDiv);
+}
